e2c0f73df664868159a0a03f22d4edd07bd8a146
Mustious <37770012+mustious@users.noreply.github.com>
2020-03-31 15:15:40 +0100

    Add files via upload

--- Mustapha_Abdullahi_day2.py
+++ Mustapha_Abdullahi_day2.py
@@ -5,7 +5,7 @@ import random
 def password_generator(password_len: int):
     """
     Generates a random alphanumeric password based on desired length of password
-    
+
     :param password_len: desired length of password
     :var alpha_num: string of alphanumeric characters
     :type alpha_num: str
@@ -15,6 +15,10 @@ def password_generator(password_len: int):
 
     password = ""
     alpha_num = string.ascii_letters + string.digits
-    for element in range(0, password_len):
-        password += random.choice(alpha_num)
-    return password
+
+    if password_len < 8:
+        print("passwork is weak... choose atleast a password of length 8")
+    else:
+        for element in range(0, password_len):
+            password += random.choice(alpha_num)
+        return password
