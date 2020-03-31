9df5e331645dca957290a5d89b3232c2ba1aa5cd
Mustious <37770012+mustious@users.noreply.github.com>
2020-03-31 15:05:14 +0100

    Add files via upload

---
+++ Mustapha_Abdullahi_day2.py
@@ -0,0 +1,20 @@
+import string
+import random
+
+
+def password_generator(password_len: int):
+    """
+    Generates a random alphanumeric password based on desired length of password
+    
+    :param password_len: desired length of password
+    :var alpha_num: string of alphanumeric characters
+    :type alpha_num: str
+    :return: password: generated password
+    :rtype: str
+    """
+
+    password = ""
+    alpha_num = string.ascii_letters + string.digits
+    for element in range(0, password_len):
+        password += random.choice(alpha_num)
+    return password
